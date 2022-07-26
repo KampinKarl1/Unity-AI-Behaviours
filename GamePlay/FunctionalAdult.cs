e5fcfb4e9ad55e98adb9092da0126d15510380a4
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2022-07-26 12:55:31 +0200

    FunctionalAdult
    A base NPC class

---
+++ GamePlay/FunctionalAdult.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamePlay;
+
+using UnityEngine.AI; //For use of Navmesh agent
+
+public class FunctionalAdult : MonoBehaviour, IHear
+{
+    public void RespondToSound(Sound sound)
+    {
+        
+    }
+}

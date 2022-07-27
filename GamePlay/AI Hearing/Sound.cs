bd3d85dae38b9cdd8fcb548e27b44c5ed1e5457a
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2022-07-27 16:26:45 +0200

    Added different types of sounds (Dangerous, Interesting)

--- GamePlay/AI Hearing/Sound.cs
+++ GamePlay/AI Hearing/Sound.cs
@@ -4,14 +4,19 @@ namespace GamePlay
 {
     public class Sound
     {
-
-        public Sound(Vector3 _pos, float _range)
+        public enum SoundType {Default = -1, Interesting, Dangerous}; 
+        
+        public Sound(Vector3 _pos, float _range, SoundType _type = SoundType.Default)
         {
+            soundType = _type;
+            
             pos = _pos;
 
             range = _range;
         }
-
+        
+        public readonly SoundType soundType;
+        
         public readonly Vector3 pos;
 
         /// <summary>

67ab25e3c60ded5e2a5742cd01023c3d5e3c3999
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2022-07-27 16:36:58 +0200

    Update Throwable.cs

--- GamePlay/Elements/Throwable.cs
+++ GamePlay/Elements/Throwable.cs
@@ -23,9 +23,22 @@ namespace GamePlay
         }
 
 
-        public void MakeASound(float range)
+        /*
+        *
+        *       The following methods should be called by adding events to the onCollideWith event.
+        *       (Plug this component into the event's field and in the drop down, select Throwable/Make_______Sound(float range))
+        *
+        */
+        public void MakeAnInterestingSound(float range)
         {
-            var sound = new Sound(transform.position, range, Sound.SoundType.Interesting);
+            var sound = new Sound(transform.position, range, Sound.SoundType.Interesting); 
+            
+            Sounds.MakeSound(sound);
+        }
+
+        public void MakeADangerousSound(float range)
+        {
+            var sound = new Sound(transform.position, range, Sound.SoundType.Dangerous
             
             Sounds.MakeSound(sound);
         }
